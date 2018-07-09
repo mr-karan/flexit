c89138c2e195cc2b0be9a981dbe4e4ff8900a00c
mr-karan <karansharma1295@gmail.com>
2018-07-09 21:58:32 +0530

    use webpack template for demo

---
+++ flexit-demo/src/App.vue
@@ -0,0 +1,67 @@
+<template>
+    <section>
+    <header class="grid-header text-center">
+        <h1>Flexit</h1>
+        <h2>Simple, clean and no fuss flexbox based grid</h2>
+    </header>
+    <div id="app" class="container">
+        <div class="row">
+            <div class="one columns">
+                <div class="box-column">
+                <p>Columns</p>
+                </div>
+            </div>
+            <div class="five columns">
+                <div class="box-column">
+                <p>Columns</p>
+                </div>
+            </div>
+            <div class="six columns">
+                <div class="box-column">
+                <p>Columns</p>
+                </div>
+            </div>
+        </div>
+        <div class="row">
+            <div class="four columns">
+                <div class="box-column">
+                <p>Columns</p>
+                </div>
+            </div>
+            <div class="five columns">
+                <div class="box-column">
+                <p>Columns</p>
+                </div>
+            </div>
+            <div class="three columns">
+                <div class="box-column">
+                <p>Columns</p>
+                </div>
+            </div>
+        </div>
+        <div class="row">
+            <div class="columns">
+                <div class="box-column">
+                <p>Columns</p>
+                </div>
+            </div>
+            <div class="one column">
+                <div class="box-column">
+                <p>Columns</p>
+                </div>
+            </div>
+            <div class="one column">
+                <div class="box-column">
+                <p>Columns</p>
+                </div>
+            </div>
+        </div>
+    </div>
+    </section>
+</template>
+
+<script>
+export default {
+  name: 'App'
+}
+</script>
--- grid-demo/src/main.js
+++
@@ -1,8 +0,0 @@
-import Vue from 'vue'
-import App from './App.vue'
-import './assets/css/style.scss'
-
-new Vue({
-  el: '#app',
-  render: h => h(App)
-})
